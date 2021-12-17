08909d13fc63a3f300c0bfca43d2727d5fbce60e
PanWidmo <tomasz.baranww@gmail.com>
2021-12-17 18:56:31 +0100

    Added GlobalStyle

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,14 +1,17 @@
 import './Root.styles.js';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle.js';
 import { Wrapper } from './Root.styles';
 
 function Root() {
   return (
     <ThemeProvider theme={theme}>
-      <Wrapper>
-        <p> Projekt Inz- Start :) </p>
-      </Wrapper>
+      <GlobalStyle>
+        <Wrapper>
+          <p> Projekt Inz- Start :) </p>
+        </Wrapper>
+      </GlobalStyle>
     </ThemeProvider>
   );
 }
