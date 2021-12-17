a38fa6488eca515dd878bac859d4367d5a473d0f
PanWidmo <tomasz.baranww@gmail.com>
2021-12-17 18:27:49 +0100

    Built standard project structure

---
+++ src/views/Root.js
@@ -0,0 +1,12 @@
+import './Root.styles.js';
+import { Wrapper } from './Root.styles';
+
+function Root() {
+  return (
+    <Wrapper>
+      <p> Projekt Inz- Start :) </p>
+    </Wrapper>
+  );
+}
+
+export default Root;
---
+++ src/views/Root.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
