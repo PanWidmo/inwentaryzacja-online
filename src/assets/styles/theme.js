c4505ce1441a31ebbe3489840186daee42d6e268
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-12-22 19:38:38 +0100

    added theme and button

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -2,12 +2,19 @@ export const theme = {
   colors: {
     white: '#FFFFFF',
     black: '#000000',
+    buttonRed: '#FF3939',
+    buttonBlue: '#1AA7FF',
+    buttonGreen: '#6DD29B',
+    tabGray: '#EAE9E9',
+    tabHoverGray: '#F0F0F0',
   },
+
   fontSize: {
-    xxl: '34px',
-    xl: '24px',
-    l: '17px',
-    m: '12px',
-    s: '11px',
+    xxxl: '3rem', // 48px
+    xxl: '2.125rem', // 34px
+    xl: '1.5rem', // 24px
+    l: '1.0625rem', // 17px
+    m: '0.75rem', // 12px
+    s: '0.6875rem', // 11px
   },
 };
---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  margin: 0.9375em 0;
+  padding: 0.4375em 1.25em;
+  background-color: ${({ theme, name }) => {
+    if (name === 'red') return theme.colors.buttonRed;
+    if (name === 'red2') return theme.colors.buttonRed;
+    if (name === 'green') return theme.colors.buttonGreen;
+    if (name === 'blue') return theme.colors.buttonBlue;
+  }};
+  position: absolute;
+  top: ${({ where }) => {
+    if (where === '1') return '3em';
+  }};
+  bottom: ${({ where }) => {
+    if (where === '3') return '2.5em';
+    if (where === '4') return '2.5em';
+    if (where === '5') return '2.5em';
+  }};
+  right: ${({ where }) => {
+    if (where === '1') return '3.8em';
+    if (where === '3') return '100vw-3.125em';
+    if (where === '4') return '3.8em';
+    if (where === '5') return '12.5em';
+  }};
+  border-radius: 0.3125em;
+  border: none;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.white};
+  cursor: pointer;
+  box-shadow: 0 0.1875em 0.5em rgba(0, 0, 0, 0.3);
+  :active {
+    transform: translateY(0.25em);
+    border: none;
+  }
+`;
