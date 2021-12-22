c75e7a272a5d5dd70e94e12392eb6c6fa17a0f6c
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-12-22 19:50:15 +0100

    refactor code

--- src/mocks/data/users.js
+++ src/mocks/data/users.js
@@ -1,8 +1,8 @@
 export const users = [
-    { lp: '1', imie: 'Zdenek', nazwisko: 'Smetana', email: 'zdenek.smetana@niewiem.pl' },
-    { lp: '2', imie: 'Maciek', nazwisko: 'Kalaput', email: 'maciek.kalaput@niewiem.pl' },
-    { lp: '3', imie: 'Marlena', nazwisko: 'Kolano', email: 'marlena.kolano@niewiem.pl' },
-    { lp: '4', imie: 'Lukas', nazwisko: 'Skywalker', email: 'lukas.skywalker@niewiem.pl' },
-    { lp: '5', imie: 'Misiu', nazwisko: 'Kolorowy', email: 'misiu.kolorowy@niewiem.pl' },
-    { lp: '6', imie: 'Kamil', nazwisko: 'Klocek', email: 'kamil.klocek@niewiem.pl' },
-  ];
+  { lp: '1', imie: 'Zdenek', nazwisko: 'Smetana', email: 'zdenek.smetana@niewiem.pl' },
+  { lp: '2', imie: 'Maciek', nazwisko: 'Kalaput', email: 'maciek.kalaput@niewiem.pl' },
+  { lp: '3', imie: 'Marlena', nazwisko: 'Kolano', email: 'marlena.kolano@niewiem.pl' },
+  { lp: '4', imie: 'Lukas', nazwisko: 'Skywalker', email: 'lukas.skywalker@niewiem.pl' },
+  { lp: '5', imie: 'Misiu', nazwisko: 'Kolorowy', email: 'misiu.kolorowy@niewiem.pl' },
+  { lp: '6', imie: 'Kamil', nazwisko: 'Klocek', email: 'kamil.klocek@niewiem.pl' },
+];
