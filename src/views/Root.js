96af6d9dac394f015420dd06a104f15063d0379a
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-01-05 00:40:05 +0100

    bit cleaning

--- src/views/Root.js
+++ src/views/Root.js
@@ -6,8 +6,8 @@ import { theme } from 'assets/styles/theme';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { Wrapper } from 'views/Root.styles';
 
-import APUsers from 'components/organisms/APUsers/APUsers';
-import APFixedAssets from 'components/organisms/APFixedAssets/APFixedAssets';
+import APUsers from 'views/APUsers';
+import APFixedAssets from 'views/APFixedAssets';
 import LoginScreen from 'views/LoginScreen';
 import ResetPassword from 'views/ResetPassword';
 import ResetPasswordConfirmation from 'views/ResetPasswordConfirmation';
