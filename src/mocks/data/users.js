af800fa84be39843f5b7223734ed3c69865a25a1
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-01-06 13:36:36 +0100

    users upgrade

--- src/mocks/data/users.js
+++ src/mocks/data/users.js
@@ -1,4 +1,10 @@
 export const users = [
+  // { Lp: '1', Imie: 'Zdenek', Nazwisko: 'Smetana', email: 'zdenek.smetana@niewiem.pl' },
+  // { Lp: '2', Imie: 'Maciek', Nazwisko: 'Kalaput', email: 'maciek.kalaput@niewiem.pl' },
+  // { Lp: '3', Imie: 'Marlena', Nazwisko: 'Kolano', email: 'marlena.kolano@niewiem.pl' },
+  // { Lp: '4', Imie: 'Lukas', Nazwisko: 'Skywalker', email: 'lukas.skywalker@niewiem.pl' },
+  // { Lp: '5', Imie: 'Misiu', Nazwisko: 'Kolorowy', email: 'misiu.kolorowy@niewiem.pl' },
+  // { Lp: '6', Imie: 'Kamil', Nazwisko: 'Klocek', email: 'kamil.klocek@niewiem.pl' },
   { a: '1', b: 'Zdenek', c: 'Smetana', d: 'zdenek.smetana@niewiem.pl' },
   { a: '2', b: 'Maciek', c: 'Kalaput', d: 'maciek.kalaput@niewiem.pl' },
   { a: '3', b: 'Marlena', c: 'Kolano', d: 'marlena.kolano@niewiem.pl' },
