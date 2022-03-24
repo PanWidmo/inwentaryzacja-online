e417b6495478a5e0754cad7bc8b2670613006e8b
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-03-24 16:11:00 +0100

    Table LP fix

--- src/components/atoms/Table/Table.js
+++ src/components/atoms/Table/Table.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { StyledTable } from './Table.styles';
 
-const Table = ({ dane, headders }) => {
+const Table = ({ dane }) => {
+  const [sortedField, setSortedField] = useState(null);
+  const headers = dane.map((header) => Object.keys(header));
+
+  let sortedData = [...dane];
+  if (sortedField !== null) {
+    sortedData.sort((a, b) => {
+      if (a[sortedField['header']] < b[[sortedField['header']]]) {
+        return -1;
+      }
+      if (a[sortedField['header']] > b[[sortedField['header']]]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
   return (
     <StyledTable>
       <thead>
         <tr>
-          {headders.map((headder) => (
-            <th key={headder}>{headder}</th>
+          <th>Lp</th>
+          {headers[1].map((header) => (
+            <th key={header}>
+              <div key={header + 1} type="button" onClick={() => setSortedField({ header })}>
+                {header[0].toUpperCase() + header.substring(1)}
+              </div>
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {dane.map((daneData) => (
-          <tr key={daneData.a}>
-            <td>{daneData.a}</td>
-            <td>{daneData.b}</td>
-            <td>{daneData.c}</td>
-            <td>{daneData.d}</td>
-            <td>{daneData.e}</td>
+        {sortedData.map((daneData, index) => (
+          <tr key={daneData.lp}>
+            <td key={index + 1}>{index + 1}</td>
+            {Object.values(daneData).map((columnData, index) => (
+              <>
+                <td key={index + 100}>{columnData}</td>
+              </>
+            ))}
           </tr>
         ))}
       </tbody>
