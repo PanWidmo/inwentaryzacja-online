032f3a3bb9cb6cdd2f2c4a70af3f00801c5810ea
PanWidmo <tomasz.baranww@gmail.com>
2022-07-08 16:50:54 +0200

    Added func to ButtonCreateFixedAsset

--- src/components/molecules/Buttons/ButtonCreateFixedAsset.js
+++ src/components/molecules/Buttons/ButtonCreateFixedAsset.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { StyledButton } from 'components/molecules/Buttons/Button.styles';
+import { useNavigate } from 'react-router-dom';
 
 export const ButtonCreateFixedAsset = ({ text = 'Dodaj' }) => {
+  const navigate = useNavigate();
+
+  const navigateToCreateFixedAsset = () => {
+    navigate('/fixed-asset-management/create');
+  };
+
   return (
-    <StyledButton name="createFixedAsset" onClick={() => alert('dziaua TWORZENIE NOWEGO FIXED ASSET button :)')}>
+    <StyledButton name="createFixedAsset" onClick={navigateToCreateFixedAsset}>
       {text}
     </StyledButton>
   );
