768c2c44acd0b9bcb7eef0d0b2a9e66c88f825e7
PanWidmo <tomasz.baranww@gmail.com>
2022-07-08 12:28:01 +0200

    Changed ButtonDownloadData message

--- src/components/molecules/Buttons/ButtonDownloadData.js
+++ src/components/molecules/Buttons/ButtonDownloadData.js
@@ -3,7 +3,7 @@ import { StyledButton } from 'components/molecules/Buttons/Button.styles';
 
 export const ButtonDownloadData = ({ text = 'Pobierz' }) => {
   return (
-    <StyledButton name="downloadData" onClick={() => alert('dziaua POBIERANIE button :)')}>
+    <StyledButton name="downloadData" onClick={() => alert('Functionality in progress... :)')}>
       {text}
     </StyledButton>
   );
