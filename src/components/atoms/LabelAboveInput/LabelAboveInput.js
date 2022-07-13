eac2c9f19d00a8283e6a3aed360e41554a21730f
PanWidmo <tomasz.baranww@gmail.com>
2022-07-13 23:04:19 +0200

    Fix blue color on LabelAboveInput component

--- src/components/atoms/LabelAboveInput/LabelAboveInput.js
+++ src/components/atoms/LabelAboveInput/LabelAboveInput.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LabelAboveInput = styled.label`
   display: flex;
   flex-direction: column;
-  color: ${({ theme }) => theme.colors.buttonBlue};
+  color: ${({ theme }) => theme.colors.blue};
   font-size: ${({ theme }) => theme.fontSize.l};
   margin: 0 0 0.25em 0;
 `;
