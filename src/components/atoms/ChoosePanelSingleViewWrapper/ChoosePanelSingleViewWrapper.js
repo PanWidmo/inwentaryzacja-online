5ac870ff7b928255b43f3253f0d9ef323ffe9988
PanWidmo <tomasz.baranww@gmail.com>
2022-07-14 12:58:46 +0200

    Improvements in ChoosePanelSingleViewWrapper

--- src/components/atoms/ChoosePanelSingleViewWrapper/ChoosePanelSingleViewWrapper.js
+++ src/components/atoms/ChoosePanelSingleViewWrapper/ChoosePanelSingleViewWrapper.js
@@ -2,18 +2,24 @@ import styled from 'styled-components';
 
 export const ChoosePanelSingleViewWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.tabHoverGray};
-  border-radius: 0.5em;
-  box-shadow: 0 0.1875em 0.5em rgba(0, 0, 0, 0.3);
-  padding: 2em 5em;
+  border-radius: 0.5rem;
+  box-shadow: 0 0.1875rem 0.5rem rgba(0, 0, 0, 0.3);
+  padding: 2rem 4rem;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   justify-content: space-evenly;
   margin: 0 auto;
   min-height: 50vh;
+  max-width: 25rem;
 
   img {
     width: 8rem;
-    margin: 2em auto;
+    margin: 2rem auto;
+  }
+
+  p {
+    text-align: center;
+    font-size: ${({ theme }) => theme.fontSize.l};
   }
 `;
