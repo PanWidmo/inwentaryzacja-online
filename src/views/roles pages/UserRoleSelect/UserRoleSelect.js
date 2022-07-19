2d28bf68f82f8950b92e2d5e1a75f11b23837fd2
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-07-19 11:59:44 +0200

    fix link to user assets

--- src/views/roles pages/UserRoleSelect/UserRoleSelect.js
+++ src/views/roles pages/UserRoleSelect/UserRoleSelect.js
@@ -9,43 +9,43 @@ import { Button } from 'components/molecules/Button/Button';
 import { useNavigate } from 'react-router-dom';
 
 export const UserRoleSelect = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navigateToUserPanel = () => {
-        // DO ZMIANY ADRES PO ZROBIENIU ODPOWIEDNIEGO KOMPONENTU!
-        navigate('/user-management');
-    };
+  const navigateToUserPanel = () => {
+    // DO ZMIANY ADRES PO ZROBIENIU ODPOWIEDNIEGO KOMPONENTU!
+    navigate('/user/summary');
+  };
 
-    const navigateToAdminPanel = () => {
-        navigate('/admin-panel');
-    };
+  const navigateToAdminPanel = () => {
+    navigate('/admin-panel');
+  };
 
-    const navigateToAccountantPanel = () => {
-        navigate('/accountant-panel');
-    };
+  const navigateToAccountantPanel = () => {
+    navigate('/accountant-panel');
+  };
 
-    return (
-        <>
-            <Header title="Wybierz swoja role" companyName="Compolexos" hasLogoutButton />
-            <ContentWrapper>
-                <ChoosePanelSingleViewWrapper as="form">
-                    <img src={userIcon} alt="people_logo" />
-                    <Button name="navigateTo" text="Uzytkownik" onClick={navigateToUserPanel} />
-                    <p>Panel uzytkownika</p>
-                </ChoosePanelSingleViewWrapper>
+  return (
+    <>
+      <Header title="Wybierz swoja role" companyName="Compolexos" hasLogoutButton />
+      <ContentWrapper>
+        <ChoosePanelSingleViewWrapper as="form">
+          <img src={userIcon} alt="people_logo" />
+          <Button name="navigateTo" text="Uzytkownik" onClick={navigateToUserPanel} />
+          <p>Panel uzytkownika</p>
+        </ChoosePanelSingleViewWrapper>
 
-                <ChoosePanelSingleViewWrapper as="form">
-                    <img src={adminIcon} alt="people_logo" />
-                    <Button name="navigateTo" text="Admin" onClick={navigateToAdminPanel} />
-                    <p>Panel administratora</p>
-                </ChoosePanelSingleViewWrapper>
+        <ChoosePanelSingleViewWrapper as="form">
+          <img src={adminIcon} alt="people_logo" />
+          <Button name="navigateTo" text="Admin" onClick={navigateToAdminPanel} />
+          <p>Panel administratora</p>
+        </ChoosePanelSingleViewWrapper>
 
-                <ChoosePanelSingleViewWrapper as="form">
-                    <img src={accountantIcon} alt="people_logo" />
-                    <Button name="downloadData" text="Ksiegowy" onClick={navigateToAccountantPanel} />
-                    <p>Panel ksiegowego</p>
-                </ChoosePanelSingleViewWrapper>
-            </ContentWrapper>
-        </>
-    );
+        <ChoosePanelSingleViewWrapper as="form">
+          <img src={accountantIcon} alt="people_logo" />
+          <Button name="downloadData" text="Ksiegowy" onClick={navigateToAccountantPanel} />
+          <p>Panel ksiegowego</p>
+        </ChoosePanelSingleViewWrapper>
+      </ContentWrapper>
+    </>
+  );
 };
