4f59a42eff4f9906bcabeb673b50a89c192c8e6f
PanWidmo <tomasz.baranww@gmail.com>
2022-11-24 16:06:54 +0100

    Created api requests file and axios.js

---
+++ src/api/requests.js
@@ -0,0 +1,8 @@
+export const requests = {
+  getFixedAssets: '/api/asset',
+  singleFixedAsset: '/api/asset',
+  getInventories: '/api/inventory/onlyall',
+  singleInventory: '/api/inventory',
+  getUsers: '/api/user/onlyall',
+  singleUser: '/api/user',
+};
