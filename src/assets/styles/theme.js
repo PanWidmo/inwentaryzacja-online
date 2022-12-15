f6cb6103d858ca634517a351893289846053397b
PanWidmo <tomasz.baranww@gmail.com>
2022-12-15 21:04:23 +0100

    Add breakpoints for media queries

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,3 +1,8 @@
+const size = {
+  mobile: '600px',
+  tablet: '1024px',
+};
+
 export const theme = {
   colors: {
     white: '#FFFFFF',
@@ -18,5 +23,13 @@ export const theme = {
     l: '1.7rem',
     m: '1.2rem',
     s: '1.1rem',
+  },
+
+  breakpoints: {
+    mobileOnly: `only screen and (max-width: ${size.mobile})`,
+    notMobile: `only screen and (min-width: ${size.mobile})`,
+    tabletOnly: `only screen and (min-width: ${size.mobile}) and (max-width ${size.tablet})`,
+    notDesktop: `only screen and (max-width: ${size.tablet})`,
+    desktopOnly: `only screen and (min-width: ${size.tablet})`,
   },
 };
