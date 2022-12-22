93f0cad898e026b4b60abbe7a94e55ff3141833e
PanWidmo <tomasz.baranww@gmail.com>
2022-12-22 14:02:50 +0100

    Add func for fixed asset create and edit

--- src/views/fixed-asset/FixedAssetCreate/FixedAssetCreate.js
+++ src/views/fixed-asset/FixedAssetCreate/FixedAssetCreate.js
@@ -8,34 +8,27 @@ import { FormField } from 'components/molecules/FormField/FormField';
 import { Footer } from 'components/organisms/Footer/Footer';
 import { Form } from 'components/organisms/Form/Form';
 import { useEffect, useState } from 'react';
-import { FormSelect } from 'components/molecules/FormSelect/FormSelect';
+import { FormSelect } from 'components/molecules/FormSelects/FormSelect';
+import { LoadingOrError } from 'components/molecules/LoadingOrError/LoadingOrError';
 
 const validate = (values) => {
   const errors = {};
   if (!values.name) {
     errors.name = 'Pole wymagane';
-  } else if (values.name.length < 2) {
-    errors.name = 'Must be 2 characters or more';
+  } else if (values.name.length < 3) {
+    errors.name = 'Wymagane minimum 3 znaki';
   }
 
   if (!values.serialNumber) {
     errors.serialNumber = 'Pole wymagane';
-  } else if (values.serialNumber.length < 2) {
-    errors.serialNumber = 'Must be 2 characters or more';
+  } else if (values.serialNumber.length < 3) {
+    errors.serialNumber = 'Wymagane minimum 3 znaki';
   }
 
   if (!values.description) {
     errors.description = 'Pole wymagane';
-  } else if (values.description.length < 2) {
-    errors.description = 'Must be 2 characters or more';
-  }
-
-  if (!values.userId) {
-    errors.userId = 'Pole wymagane';
-  }
-
-  if (!values.inventoryId) {
-    errors.inventoryId = 'Pole wymagane';
+  } else if (values.description.length < 3) {
+    errors.description = 'Wymagane minimum 3 znaki';
   }
 
   return errors;
@@ -47,8 +40,13 @@ export const FixedAssetCreate = () => {
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
+  const navigateToFixedAsset = () => {
+    navigate(requests.fixedAssetManagement);
+  };
+
   const getData = async () => {
     setLoading(true);
+
     try {
       const result = await axios.get(requests.singleUser, {
         headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}` },
@@ -61,23 +59,12 @@ export const FixedAssetCreate = () => {
     setLoading(false);
   };
 
-  useEffect(() => {
-    getData();
-    // eslint-disable-next-line
-  }, []);
-
-  const navigateToFixedAsset = () => {
-    navigate('/fixed-asset-management');
-  };
-
   const formik = useFormik({
     initialValues: {
       name: '',
-      inventoryNumber: '',
       serialNumber: '',
-      description: '',
       userId: '',
-      inventoryId: '',
+      description: '',
     },
     validate,
     onSubmit: (values) => {
@@ -93,53 +80,55 @@ export const FixedAssetCreate = () => {
     },
   });
 
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
-      {data.map((el) => {
-        return (
-          <option key={el.id} value={el.id}>
-            {el.name} {el.surname}
-          </option>
-        );
-      })}
       <Header title="Dodanie nowego środka trwałego" hasLogoutButton />
       <ContentWrapper>
-        <Form id="fixedAssetCreateForm" onSubmit={formik.handleSubmit}>
-          <FormField
-            label="Nazwa"
-            id="name"
-            name="name"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            onBlur={formik.handleBlur}
-            error={formik.touched.name && formik.errors.name ? formik.errors.name : null}
-          />
+        {!loading && !error ? (
+          <Form id="fixedAssetCreateForm" onSubmit={formik.handleSubmit}>
+            <FormField
+              label="Nazwa"
+              id="name"
+              name="name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              onBlur={formik.handleBlur}
+              error={formik.touched.name && formik.errors.name ? formik.errors.name : null}
+            />
 
-          <FormField
-            label="Numer Seryjny"
-            id="serialNumber"
-            name="serialNumber"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.serialNumber}
-            onBlur={formik.handleBlur}
-            error={formik.touched.serialNumber && formik.errors.serialNumber ? formik.errors.serialNumber : null}
-          />
+            <FormField
+              label="Numer Seryjny"
+              id="serialNumber"
+              name="serialNumber"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.serialNumber}
+              onBlur={formik.handleBlur}
+              error={formik.touched.serialNumber && formik.errors.serialNumber ? formik.errors.serialNumber : null}
+            />
 
-          <FormSelect label="Użytkownik" id="userId" name="userId" value={formik.values.userId} onChange={formik.handleChange} data={data} />
+            <FormSelect label="Osoba przypisana" id="userId" name="userId" value={formik.values.userId} onChange={formik.handleChange} data={data} />
 
-          <FormField
-            label="Opis"
-            id="description"
-            name="description"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.description}
-            onBlur={formik.handleBlur}
-            error={formik.touched.description && formik.errors.description ? formik.errors.description : null}
-          />
-        </Form>
+            <FormField
+              label="Opis"
+              id="description"
+              name="description"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.description}
+              onBlur={formik.handleBlur}
+              error={formik.touched.description && formik.errors.description ? formik.errors.description : null}
+            />
+          </Form>
+        ) : (
+          <LoadingOrError msg={error ? error : 'Loading...'} />
+        )}
       </ContentWrapper>
       <Footer hasBackToPrevPageButton hasCreateFixedAssetButton />
     </>
--- src/views/fixed-asset/FixedAssetEdit/FixedAssetEdit.js
+++ src/views/fixed-asset/FixedAssetEdit/FixedAssetEdit.js
@@ -9,25 +9,20 @@ import { useFormik } from 'formik';
 import { LoadingOrError } from 'components/molecules/LoadingOrError/LoadingOrError';
 import { Footer } from 'components/organisms/Footer/Footer';
 import { Form } from 'components/organisms/Form/Form';
+import { FormSelect } from 'components/molecules/FormSelects/FormSelect';
 
 const validate = (values) => {
   const errors = {};
   if (!values.name) {
     errors.name = 'Pole wymagane';
-  } else if (values.name.length < 2) {
-    errors.name = 'Must be 2 characters or more';
-  }
-
-  if (!values.inventoryNumber) {
-    errors.inventoryNumber = 'Pole wymagane';
-  } else if (values.inventoryNumber.length < 2) {
-    errors.inventoryNumber = 'Must be 2 characters or more';
+  } else if (values.name.length < 3) {
+    errors.name = 'Wymagane minimum 3 znaki';
   }
 
   if (!values.serialNumber) {
     errors.serialNumber = 'Pole wymagane';
-  } else if (values.serialNumber.length < 2) {
-    errors.serialNumber = 'Must be 2 characters or more';
+  } else if (values.serialNumber.length < 3) {
+    errors.serialNumber = 'Wymagane minimum 3 znaki';
   }
 
   if (!values.description) {
@@ -36,18 +31,11 @@ const validate = (values) => {
     errors.description = 'Must be 2 characters or more';
   }
 
-  if (!values.userId) {
-    errors.userId = 'Pole wymagane';
-  }
-
-  if (!values.inventoryId) {
-    errors.inventoryId = 'Pole wymagane';
-  }
-
   return errors;
 };
 export const FixedAssetEdit = () => {
   const { id } = useParams();
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
@@ -55,15 +43,48 @@ export const FixedAssetEdit = () => {
   const navigateToFixedAsset = () => {
     navigate(requests.fixedAssetManagement);
   };
+  const getUsers = async () => {
+    setLoading(true);
 
+    try {
+      const result = await axios.get(requests.singleUser, {
+        headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}` },
+      });
+      setData(result.data);
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  const getData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(`${requests.singleFixedAsset}/${id}`, {
+        headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}` },
+      });
+      const data = response.data;
+
+      await formik.setValues({
+        name: data.name,
+        serialNumber: data.serialNumber,
+        userId: data.userId,
+        description: data.description,
+      });
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
   const formik = useFormik({
     initialValues: {
       name: '',
-      inventoryNumber: '',
       serialNumber: '',
-      description: '',
       userId: '',
-      inventoryId: '',
+      description: '',
     },
     validate,
     onSubmit: (values) => {
@@ -79,25 +100,8 @@ export const FixedAssetEdit = () => {
     },
   });
 
-  const getData = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get(`${requests.singleFixedAsset}/${id}`, {
-        headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}` },
-      });
-      const data = response.data;
-
-      await formik.setValues({
-        ...data,
-      });
-    } catch (error) {
-      console.error(error.message);
-      setError(error.message);
-    }
-    setLoading(false);
-  };
-
   useEffect(() => {
+    getUsers();
     getData();
     // eslint-disable-next-line
   }, []);
@@ -120,17 +124,6 @@ export const FixedAssetEdit = () => {
             />
 
             <FormField
-              label="Numer Inwentarzowy"
-              id="inventoryNumber"
-              name="inventoryNumber"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.inventoryNumber}
-              onBlur={formik.handleBlur}
-              error={formik.touched.inventoryNumber && formik.errors.inventoryNumber ? formik.errors.inventoryNumber : null}
-            />
-
-            <FormField
               label="Numer Seryjny"
               id="serialNumber"
               name="serialNumber"
@@ -141,6 +134,8 @@ export const FixedAssetEdit = () => {
               error={formik.touched.serialNumber && formik.errors.serialNumber ? formik.errors.serialNumber : null}
             />
 
+            <FormSelect label="Osoba przypisana" id="userId" name="userId" value={formik.values.userId} onChange={formik.handleChange} data={data} />
+
             <FormField
               label="Opis"
               id="description"
@@ -150,28 +145,6 @@ export const FixedAssetEdit = () => {
               value={formik.values.description}
               onBlur={formik.handleBlur}
               error={formik.touched.description && formik.errors.description ? formik.errors.description : null}
-            />
-
-            <FormField
-              label="Id Użytkownika"
-              id="userId"
-              name="userId"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.userId}
-              onBlur={formik.handleBlur}
-              error={formik.touched.userId && formik.errors.userId ? formik.errors.userId : null}
-            />
-
-            <FormField
-              label="Id Inwentaryzacji"
-              id="inventoryId"
-              name="inventoryId"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.inventoryId}
-              onBlur={formik.handleBlur}
-              error={formik.touched.inventoryId && formik.errors.inventoryId ? formik.errors.inventoryId : null}
             />
           </Form>
         ) : (
