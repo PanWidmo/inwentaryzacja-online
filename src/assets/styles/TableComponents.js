b801a9aa7e068a045f208f5f6f713d8ba513db93
PanWidmo <tomasz.baranww@gmail.com>
2022-12-28 17:20:42 +0100

    Change styles for TableComponents

--- src/assets/styles/TableComponents.js
+++ src/assets/styles/TableComponents.js
@@ -11,16 +11,25 @@ export const Wrapper = styled.div`
   h4 {
     color: ${({ theme }) => theme.colors.hGray};
   }
+
+  @media ${({ theme }) => theme.breakpoints.notDesktop} {
+    padding: 0;
+  }
 `;
 
 export const InnerWrapper = styled.div`
-  width: 80%;
+  width: fit-content;
   position: relative;
   margin-left: auto;
   margin-right: auto;
   button {
     position: relative;
     margin-top: 0.5em;
+  }
+  @media ${({ theme }) => theme.breakpoints.notDesktop} {
+    width: 100%;
+    overflow: auto;
+    padding: 1rem;
   }
 `;
 
