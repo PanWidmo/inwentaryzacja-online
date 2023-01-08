b82f61e14a863a47a247cccf220a65db185ef9fa
panwidmo <tomasz.baranww@gmail.com>
2023-01-08 18:15:26 +0100

    Add functionality to check if inventory is active

--- src/views/user/UserFixedAsset/UserFixedAsset.js
+++ src/views/user/UserFixedAsset/UserFixedAsset.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'api/axios';
+import { requests } from 'api/requests';
 import { Table } from 'components/organisms/Table/Table';
 import { Wrapper, InnerWrapper } from 'assets/styles/TableComponents';
 import { Header } from 'components/organisms/Header/Header';
@@ -8,11 +9,40 @@ import { Footer } from 'components/organisms/Footer/Footer';
 import { LoadingOrError } from 'components/molecules/LoadingOrError/LoadingOrError';
 
 export const UserFixedAsset = () => {
-  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isActiveInventory, setIsActiveInventory] = useState(false);
+  const [error, setError] = useState(null);
   const [dane, setDane] = useState([]);
 
+  const checkInventoryActive = async () => {
+    setLoading(true);
+    try {
+      // TODO: zmienic endpoint fixedAsset na inventory jak bedzie dodane isActive
+      const result = await axios.get(requests.singleFixedAsset, {
+        headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}` },
+      });
+      // TODO: to setData do wywalenia jak bedzie dzialac pobieranie assetow usera
+      setData(result.data);
+      // TODO: zmienic fixedAsset hasInventoried na inventory isActive jak bedzie dodane
+      if (result.data.some((e) => e.hasInventoried === true)) {
+        setIsActiveInventory(true);
+      }
+    } catch (error) {
+      console.error(error.message);
+
+      setError(error.message);
+    }
+
+    setLoading(false);
+  };
+
   useEffect(() => {
+    checkInventoryActive();
+    // eslint-disable-next-line
+  }, []);
+
+  /*  useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
@@ -31,7 +61,7 @@ export const UserFixedAsset = () => {
 
   useEffect(() => {
     setData(dane[0]?.assets);
-  }, [dane]);
+  }, [dane]);*/
 
   return (
     <>
@@ -39,17 +69,15 @@ export const UserFixedAsset = () => {
       <ContentWrapper>
         <Wrapper>
           <InnerWrapper>
-            {!loading ? (
-              <>
-                <Table dane={data} dataName="fixed-asset" id="usersTable" />
-              </>
+            {!loading && !error ? (
+              <Table dane={data} dataName="fixed-asset" id="fixedAssetsTable" />
             ) : (
-              <LoadingOrError />
+              <LoadingOrError msg={error ? error : 'Ładowanie...'} />
             )}
           </InnerWrapper>
         </Wrapper>
       </ContentWrapper>
-      <Footer hasBackToPrevPageButton hasConfirmUserAsset />
+      {isActiveInventory === true ? <Footer hasBackToPrevPageButton hasConfirmUserAsset /> : <Footer hasBackToPrevPageButton />}
     </>
   );
 };
